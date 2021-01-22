6a012a7491cda7487c37f20c1efc49c7fd921038
Aarfeen1 <72484730+Aarfeen1@users.noreply.github.com>
2021-01-22 18:23:42 +0530

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,24 +1,56 @@
-package com.xworkz.college.tester;
+package com.xworkz.mouse.tester;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.xworkz.mouse.dao.*;
+import com.xworkz.mouse.dto.*;
 
 
 
-import com.xworkz.college.dao.CollegeDAO;
-import com.xworkz.college.dao.CollegeDAOimpl;
-import com.xworkz.college.dto.CollegeDTO;
+public class Tester {
 
-public class Tester{
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		CollegeDTO collegeDTO = new CollegeDTO();
-		collegeDTO.setCname("TJIT");
-		collegeDTO.setLocation("BANGALORE");
-		collegeDTO.setNoOfBranches(10);
-		CollegeDAO collegeDaoImpl =new CollegeDAOimpl();
+				
+		        FileInputStream file= new FileInputStream(new File("C:\\Users\\SONY\\Desktop\\data.xlsx"));
+		        try (XSSFWorkbook workbook = new XSSFWorkbook(file)) {
+					XSSFSheet sheet =workbook.getSheetAt(0);
+					Row row;
+					for(int i=1;i<=sheet.getLastRowNum();i++) {
+						row = (Row) sheet.getRow(i);
+						
+					    if(row.getCell(0)==null) {} else
+							row.getCell(0).toString();
+						String NAME;
+						if(row.getCell(1)==null) { NAME="null";}
+						else NAME=row.getCell(1).toString();
+						String SIZE;
+						if(row.getCell(2)==null) { SIZE="null";}
+						else SIZE=row.getCell(2).toString();
+						
+						MouseDTO mousedto=new MouseDTO();
+						mousedto.setName(NAME);
+mousedto.setSize(SIZE);		        	
+	
+						
+				
+						  MouseDAO mousedao=new MouseDAOImp();
+					   
+						mousedao.saveMouse(mousedto);
+
+					}
+				}
+		        file.close();
+				
+			}
+					  
+
 		
-		collegeDaoImpl.saveCollege(collegeDTO);
-		//collegeDaoImpl.fetchCollege(2);
-		//collegeDaoImpl.updateCollege(collegeDTO,2);
-		
+
 	}
-}
+
+
